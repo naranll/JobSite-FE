c4b4258e8b52fa5b7b246c1cab58ec7dd78b5767
Morning <morning@admins-iMac-3.local>
2023-03-28 11:57:39 +0800

    bodyStyle

--- src/applicant/JobBoard.jsx
+++ src/applicant/JobBoard.jsx
@@ -1,7 +1,21 @@
 //Job board дээрх job cards-уудыг гаргаж ирэх
+import "../styles/jobBoard.css";
 
-export default function JobBoard(){
-    return(<div>
-        Applicant Job Board
-    </div>)
+export default function JobBoard() {
+  return (
+    <div className="JobBody">
+      <header>
+        <div className="header">header</div>
+      </header>
+      <div className="mainbody">
+        <div className="filter">filter</div>
+        <main>
+          <div className="">main</div>
+        </main>
+      </div>
+      <footer>
+        <div className="footer">footer</div>
+      </footer>
+    </div>
+  );
 }
